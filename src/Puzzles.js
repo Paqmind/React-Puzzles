43247ee3ea24a33fee1511e7bc56b5ddb214f6c0
pavel <paveltextfree@gmail.com>
2018-02-24 12:21:10 +0300

    add null

--- src/Puzzles.js
+++ src/Puzzles.js
@@ -12,7 +12,7 @@ class Puzzles extends React.Component{
                 1, 2, 3,
                 4, 5, 6,
                 7, 8, 9,
-                10, 11, ""
+                10, 11, null
             ],
             isStarted : false,
         }
